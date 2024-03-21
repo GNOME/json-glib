ea7269e1cfcc360feaeb35f8e4528ca740b171c2
Emmanuele Bassi <ebassi@gnome.org>
2024-03-21 00:27:10 +0000

    docs: Add URL map for cross-linking

---
+++ doc/urlmap.js
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Emmanuele Bassi
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+]
